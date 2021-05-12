0f9eef6d76ff30511ddc0424010d718a6b4ad217
Kai Qi <173136u@mymail.nyp.edu.sg>
2021-05-12 14:34:30 +0800

    Update DoorV1 open/close

---
+++ Madhouse Before 12/Assets/Scripts/DoorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator doorAnim;
+    private void OnTriggerEnter(Collider other) {
+        doorAnim.SetBool("isOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        doorAnim.SetBool("isOpening", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+}
