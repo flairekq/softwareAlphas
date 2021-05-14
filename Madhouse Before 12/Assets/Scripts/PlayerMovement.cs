c3e85d2fcef78500dbc70e0014aaef5d0e793415
Kai Qi <173136u@mymail.nyp.edu.sg>
2021-05-14 14:13:54 +0800

    Update movement of player

--- Madhouse Before 12/Assets/Scripts/PlayerMovement.cs
+++ Madhouse Before 12/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private CharacterController controller; 
+
+    private float speed = 6f;
+    private float verticalVelocity;
+    private float gravity = 9.81f;
+    private float jumpForce = 3.5f;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        if(controller.isGrounded) {
+            verticalVelocity = -gravity * Time.deltaTime;
+
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                verticalVelocity = jumpForce;
+            }
+            
+        } else {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+
+        // transform.right takes the direction that the player is facing and goes to the right
+        // transform.forward also takes the direction that the player is facing and goes forward
+        Vector3 move = transform.right * x + transform.forward * z;
+        // multiply by Time.deltaTime to be frame rate independent
+        controller.Move(move * speed * Time.deltaTime);
+
+
+        controller.Move(moveVector  * Time.deltaTime);
+
+    }
+
+    /*
     public CharacterController controller;
 
     public float speed = 8f;
@@ -54,5 +94,5 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-    }
+    } */
 }
