2f52946b785e01067cc799ec3395a2528e0ced66
Jingwencloud <jingwen098765432@gmail.com>
2021-06-04 01:01:42 +0800

    Update hand rigging

--- Madhouse Before 12/Assets/Scripts/RifleManager.cs
+++ Madhouse Before 12/Assets/Scripts/RifleManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class RifleManager : MonoBehaviour
 {
 
-
     public int bulletspermag = 10;
     public int totalbulletsleft = 70;
 
@@ -13,61 +12,44 @@ public class RifleManager : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public Transform shootPoint;
-
-    public Transform shootPointIdle;
-    public Camera fpsCamera;
-
-    public CameraShake cameraShake;
+    //public CameraShake cameraShake;
 
     public ParticleSystem muzzleFlash;
-    public LayerMask enemyMask;
 
-    private Animator anim;
+    //private Animator anim;
 
-    public GameObject scopeOverlay;
-
-    public GameObject weaponCamera;
-
-    public Camera mainCamera;
-
-    public float scopedFOV = 15f;
-    private float normalFOV;
-
-    //public GameObject impactEffect;
 
     private int isShooting = Animator.StringToHash("isShooting");
    // private int isIdle = Animator.StringToHash("isIdle");
-
-    private bool isScoped= false;
 
 
     private void Start()
     { 
         currentBullets = bulletspermag;
-         anim = GetComponent<Animator>();
+         //anim = GetComponent<Animator>();
     }
 
     void Update() {
          if(Input.GetMouseButtonDown(0)) {
-            Aim();
+           // Aim();
             
         }
         if(Input.GetMouseButtonDown(1))
         {
             if(currentBullets <= 0 && totalbulletsleft>0) {
-                Reload();
+                //Reload();
                 currentBullets = bulletspermag;
                 totalbulletsleft -= bulletspermag;
             } else {
             Shoot();
-             StartCoroutine(cameraShake.Shake(0.15f, 0.05f));
+            // StartCoroutine(cameraShake.Shake(0.15f, 0.05f));
             }
            
-        } else {
+        }/* else {
             anim.SetBool("isIdle",true);
             anim.SetBool(isShooting, false);
             anim.SetBool("isReload", false);
-        } 
+        } */
     }
 
     void Shoot()
@@ -75,7 +57,6 @@ public class RifleManager : MonoBehaviour
         muzzleFlash.Play();
         RaycastHit hit;
         currentBullets--;
-        if(isScoped) {
         if(Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range))
         {   
             Debug.Log(hit.transform.name);
@@ -84,67 +65,7 @@ public class RifleManager : MonoBehaviour
                 target.TakeDamage(damage);
             }
         }
-        } else {
-            if(Physics.Raycast(shootPointIdle.position, shootPointIdle.transform.forward, out hit, range))
-        {   
-            Debug.Log(hit.transform.name);
-            Target target = hit.transform.GetComponent<Target>();
-            if(target != null) {
-                target.TakeDamage(damage);
-            }
-
-            /*GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactGO, 2f);
-            */
-        }
+    } 
+    
     }
-    }
-     private void Aim() 
-    {   
-        isScoped = !isScoped;
-        anim.SetBool(isShooting, isScoped);
-
-        if(isScoped) {
-            StartCoroutine(OnScoped());
-        } else {
-            OnUnscoped();
-        }
-        
-    }
-
-    void OnUnscoped() 
-    {
-        scopeOverlay.SetActive(false);
-        weaponCamera.SetActive(true);
-
-        mainCamera.fieldOfView = normalFOV;
-    }
-
-    IEnumerator OnScoped() 
-    {
-        yield return new WaitForSeconds(0.15f);
-
-        scopeOverlay.SetActive(true);
-        weaponCamera.SetActive(false);
-        normalFOV = mainCamera.fieldOfView;
-        mainCamera.fieldOfView = 15f;
-    }
-
-    void Reload()
-    {
-        
-        if(isScoped) {
-            isScoped = false; 
-            OnUnscoped(); 
-            anim.SetBool("isReload", true);
-            anim.SetBool("isShooting", false);
-            anim.SetBool("isIdle", false);
-        } else {
-            anim.SetBool("isReload", true);
-            anim.SetBool("isShooting", false);
-            anim.SetBool("isIdle", false);
-        }
-    }
-
-
-}
+  
