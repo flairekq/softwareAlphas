8a05a40ed860cdc33740552a00f576b29f8e863d
Jingwencloud <jingwen098765432@gmail.com>
2021-06-19 19:35:02 +0800

    Update player8 prefab

---
+++ Madhouse Before 12/Assets/RifleManager2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RifleManager2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform CameraPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.rotation = CameraPos.transform.rotation;
+        
+    }
+}
