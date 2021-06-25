58964da00b700f1313e75d7cf298b40e41af721d
Kai Qi <173136u@mymail.nyp.edu.sg>
2021-06-25 20:35:40 +0800

    Fixed bug of clue text appearing on note text vice versa

--- Madhouse Before 12/Assets/Scripts/Items/Item.cs
+++ Madhouse Before 12/Assets/Scripts/Items/Item.cs
@@ -8,23 +8,36 @@ public class Item : MonoBehaviour
     [SerializeField] public string content = "";
     [SerializeField] private Sprite onExpandIcon;
     private Text noteText = null;
+    private Text clueText = null;
     private Text displayNameText = null;
     private Image focusPanelImage = null;
 
-    public void Examine(Text nt, Text dnt, Image fpi)
+    public void Examine(Text nt, Text ct, Text dnt, Image fpi, string type)
     {
         noteText = nt;
+        clueText = ct;
         displayNameText = dnt;
         focusPanelImage = fpi;
 
         if (content != "")
         {
-            noteText.text = content;
-            noteText.enabled = true;
+            // noteText.text = content;
+            // noteText.enabled = true;
+            if (type.Equals("DiaryClue"))
+            {
+                noteText.enabled = false;
+                clueText.text = content;
+                clueText.enabled = true;
+            } else if (type.Equals("Note")) {
+                noteText.text = content;
+                noteText.enabled = true;
+                clueText.enabled = false;
+            }
         }
         else
         {
             noteText.enabled = false;
+            clueText.enabled = false;
         }
 
         focusPanelImage.sprite = onExpandIcon;
@@ -33,17 +46,21 @@ public class Item : MonoBehaviour
         displayNameText.enabled = true;
     }
 
-    public void OffExamine(Text nt, Text dnt, Image fpi) {
+    public void OffExamine(Text nt, Text ct, Text dnt, Image fpi)
+    {
         noteText = nt;
+        clueText = ct;
         displayNameText = dnt;
         focusPanelImage = fpi;
-        
+
         noteText.enabled = false;
+        clueText.enabled = false;
         focusPanelImage.enabled = false;
         displayNameText.enabled = false;
     }
 
-    public Sprite GetExpandIcon() {
+    public Sprite GetExpandIcon()
+    {
         return onExpandIcon;
     }
 }
