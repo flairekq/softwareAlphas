33608695624d293e980b4c4c8535cb3b0fd25c0a
Kai Qi <173136u@mymail.nyp.edu.sg>
2021-07-15 22:18:12 +0800

    Debugging monster management bug

--- Madhouse Before 12/Assets/Scripts/CirclePlayer/AttackBehaviour.cs
+++ Madhouse Before 12/Assets/Scripts/CirclePlayer/AttackBehaviour.cs
@@ -26,7 +26,7 @@ public class AttackBehaviour : StateMachineBehaviour
         EController2 currentController = current.GetComponent<EController2>();
         if (currentController.PV.IsMine)
         {
-            currentController.AttackedPlayer();
+            // currentController.AttackedPlayer();
             TimerManagement.instance.AdjustTime(5, false);
             currentController.isAttacking = false;
         }
